b5819429c602c648ccb54fffb29f0505216e17f3
Vern Saeteurn <vernchiem2000@gmail.com>
2022-01-22 00:11:02 -0800

    125. Updating state with fetched data

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -5,7 +5,8 @@ const KEY = "AIzaSyDCu70qfFcJZyK8GQKQXzOtfU06sanl4as"
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    part: snippet,
+    part: 'snippet',
+    type: 'video',
     maxResults: 5,
     key: KEY
   }
